57cf51578950d12846d6e4c15aa98ae7dfd4a225
Hideaki Kanai <ifrit.anplosia@gmail.com>
2020-05-19 10:24:38 +0900

    Initial Commit

---
+++ RemoteTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemoteTest2
+//
+//  Created by 金井英晃 on 2020/05/19.
+//  Copyright © 2020 ifrit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
